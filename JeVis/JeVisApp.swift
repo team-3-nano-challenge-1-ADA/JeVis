326d6d1358198c1771d6c44e4b85e3ae1cd34d43
Rizki Maulana <rizki.maulana@majoo.id>
2024-04-23 13:35:23 +0700

    Initial Commit

---
+++ JeVis/JeVisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeVisApp.swift
+//  JeVis
+//
+//  Created by Rizki Maulana on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JeVisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
