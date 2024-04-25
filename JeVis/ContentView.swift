63cfe50cd2fb72ff0a183d0d17ea351786f776a0
HalizaSyafa27 <111560506+HalizaSyafa27@users.noreply.github.com>
2024-04-25 21:06:53 +0700

    Update ContentView.swift
    page 2

--- JeVis/ContentView.swift
+++ JeVis/ContentView.swift
@@ -8,17 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var showAnimationView = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack {
+            // Latar belakang dengan warna background
+            Rectangle()
+                .fill(Color.background)
+                .ignoresSafeArea(.all)
+            
+            // Konten utama termasuk logo dan gambar
+            VStack {
+                HStack {
+                    Spacer()
+                    Image("logo")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding() // Beri jarak dari tepi
+                        .onTapGesture {
+                            self.showingImagePicker = false
+                        }
+
+                    Image(systemName: "questionmark.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundColor(Color.button)
+                        .frame(width: 30, height: 30)
+                        .padding()
+                        .onTapGesture {
+                            self.showingImagePicker = false
+                        }
+                }
+                
+                // Area gambar (atau placeholder gambar)
+                if image != nil {
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.top, 200)
+                } else {
+                    Image(systemName: "photo.circle.fill")
+                        .resizable()
+                        .foregroundColor(Color.button)
+                        .frame(width: 100, height: 100)
+                        .padding(.top, 200)
+                }
+                Spacer()
+            }
+            .onTapGesture {
+                //SELECT IMAGE
+                self.showingImagePicker = true
+            }
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+                ImagePicker(image: self.$inputImage)
+            }
+            if showAnimationView {
+                AnimationView()
+            }
         }
-        .padding()
+    }
+    
+    func loadImage(){
+        guard let inputImage = inputImage else {return}
+        image = Image(uiImage: inputImage)
+        
+        showAnimationView = true
     }
 }
+
 
 #Preview {
     ContentView()
 }
+
