5e3d523d98ac37747df7a2ef84684227ae4fd398
HalizaSyafa27 <111560506+HalizaSyafa27@users.noreply.github.com>
2024-04-25 21:08:57 +0700

    Add files via upload

---
+++ JeVis/Preview Content/JeVis/JeVis/JeVisApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeVisApp.swift
+//  JeVis
+//
+//  Created by Haliza Syafa Oktaviani on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct JeVisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
