08ba95eee0093adda421c3292e974b63d2e8b97e
Rizki Maulana <rizki.maulana@majoo.id>
2024-04-26 09:31:06 +0700

    Add empty files

--- JeVis/Models/SampleModel.swift
+++ JeVis/Models/SampleModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct SampleModel {
+    let name: String
+    let type: String
+    let distance: Double
+    let weight: Double
+    let gender: String
+    
+    static func generateLeppyData() -> SampleModel {
+        let pet = SampleModel(name: "Leppy", type: "Domestic", distance: 1.0, weight: 3.0, gender: "Male")
+        return pet
+    }
+    
+    static func generateButetData() -> SampleModel {
+        let pet = SampleModel(name: "Butet", type: "Persian", distance: 3.5, weight: 5.0, gender: "Female")
+        return pet
+    }
+    
+    static func generateSkyData() -> SampleModel {
+        let pet = SampleModel(name: "Sky", type: "Domestic", distance: 8.5, weight: 4.9, gender: "Female")
+        return pet
+    }
+    
+    static func generateKentangData() -> SampleModel {
+        let pet = SampleModel(name: "Kentang", type: "British Shorthair", distance: 8.5, weight: 4.2, gender: "Female")
+        return pet
+    }
+}
+
--- JeVis/Views/HelpView.swift
+++ JeVis/Views/HelpView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct HelpView: View {
     var body: some View {
+        // TODO Tambahkan UI halaman help
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
