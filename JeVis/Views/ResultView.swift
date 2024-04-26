f9c8f257f2df72a758801b17af46332f14608b61
Rizki Maulana <rizki.maulana@majoo.id>
2024-04-26 17:51:27 +0700

    Resolve conflict

--- JeVis/Views/ResultView.swift
+++ JeVis/Views/ResultView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ResultView: View {
+    let coordinate:String
+    let imageUrl:String
+    
     var body: some View {
-        VStack {
-            AnimationView(name: "progressBar")
-                .frame(width: 600, height: 70)
-        }.padding(.top, 500)
+        ZStack {
+            Rectangle()
+                .fill(Color.background)
+                .ignoresSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                AsyncImage(url: URL(string: imageUrl)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(maxHeight: 180)
+                .padding([.bottom], 18)
+                
+                Text(coordinate)
+                    .padding([.bottom], 16)
+                
+                HStack {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "mappin.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Color.button)
+                    })
+                    Spacer()
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Image(systemName: "magnifyingglass.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Color.button)
+                    })
+                }.frame(width: 136)
+                
+                Spacer()
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Image(systemName: "speaker.wave.2.circle.fill")
+                        .resizable()
+                        .frame(width: 50, height: 50)
+                        .foregroundStyle(Color.button)
+                })
+                .padding(.bottom, 50)
+            }.padding()
+        }
     }
 }
 
 #Preview {
-    ResultView()
+    ResultView(coordinate: "7° 36′ 28\" LS, 110° 12′ 13\" BT", imageUrl: "https://gaetlokal.id/cni-content/uploads/modules/posts/20200917084844.png")
 }
