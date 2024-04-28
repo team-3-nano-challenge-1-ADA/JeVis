4d0775a78655846f734e5a26f1af33c4b9b0d2ec
Rizki Maulana <rizki.maulana@majoo.id>
2024-04-28 21:22:53 +0700

    Reformat and fix error running

--- JeVis/Views/WebView.swift
+++ JeVis/Views/WebView.swift
@@ -9,15 +9,15 @@ import WebKit
 import SwiftUI
 
 struct WebView: UIViewRepresentable {
-  let url: URL
-
-  func makeUIView(context: Context) -> WKWebView {
-    let webView = WKWebView()
-    webView.load(URLRequest(url: url))
-    return webView
-  }
-
-  func updateUIView(_ webView: WKWebView, context: Context) {
-    // Handle potential URL updates here
-  }
+    let url: URL
+    
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+    
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        // Handle potential URL updates here
+    }
 }
