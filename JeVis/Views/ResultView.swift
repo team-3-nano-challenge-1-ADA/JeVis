0aeb1f73561a726ac8013b8be8a53347707adeff
Rizki Maulana <rizki.maulana@majoo.id>
2024-04-29 16:30:23 +0700

    Fix code indentation, page back button, and remove unused comment

--- JeVis/Views/ResultView.swift
+++ JeVis/Views/ResultView.swift
@@ -20,88 +20,75 @@ struct ResultView: View {
     
     var body: some View {
         NavigationView{
-            
-        ZStack {
-            Rectangle()
-                .fill(Color.background)
-                .ignoresSafeArea(.all)
-            VStack {
-                Spacer()
-                image?
-                    .resizable()
-                    .scaledToFit()
-                    .transition(.opacity)
-                    .frame(maxHeight: 180)
-                    .padding([.bottom], 18)
-                Text("\(locationModel?.latitude ?? defaultCoordinate),\(locationModel?.longitude ?? defaultCoordinate)")
-                    .padding([.bottom], 16)
-                HStack {
-
-//                    Button(action: {}, label: {
-//                        Image(systemName: "mappin.circle.fill")
-//                            .resizable()
-//                            .frame(width: 50, height: 50)
-//                            .foregroundStyle(Color.button)
-//                    })
-                    NavigationLink(destination: MapView(locationModel: locationModel)) {
-                                        Image(systemName: "mappin.circle.fill")
-                                            .resizable()
-                                            .frame(width: 50, height: 50)
-                                            .foregroundColor(Color.button)
-                    }
+            ZStack {
+                Rectangle()
+                    .fill(Color.background)
+                    .ignoresSafeArea(.all)
+                VStack {
                     Spacer()
-                    Image(systemName: "magnifyingglass.circle.fill")
+                    image?
                         .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundStyle(Color.button)
-                        .onTapGesture {
-                            showSearchWebView = true
+                        .scaledToFit()
+                        .transition(.opacity)
+                        .frame(maxHeight: 180)
+                        .padding([.bottom], 18)
+                    Text("\(locationModel?.latitude ?? defaultCoordinate),\(locationModel?.longitude ?? defaultCoordinate)")
+                        .padding([.bottom], 16)
+                    HStack {
+                        NavigationLink(destination: MapView(locationModel: locationModel)) {
+                            Image(systemName: "mappin.circle.fill")
+                                .resizable()
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(Color.button)
                         }
-                        .sheet(isPresented: $showSearchWebView) {
-                            ZStack {
-                                WebView(url: URL(string: "\(Constants.googleLensSearch)\(locationModel?.imageUrl ?? "")")!, isLoading: $isWebviewLoading)
-                                if isWebviewLoading {
-                                    ProgressView()
+                        Spacer()
+                        Image(systemName: "magnifyingglass.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Color.button)
+                            .onTapGesture {
+                                showSearchWebView = true
+                            }
+                            .sheet(isPresented: $showSearchWebView) {
+                                ZStack {
+                                    WebView(url: URL(string: "\(Constants.googleLensSearch)\(locationModel?.imageUrl ?? "")")!, isLoading: $isWebviewLoading)
+                                    if isWebviewLoading {
+                                        ProgressView()
+                                    }
+                                    
                                 }
                                 
                             }
-                            
+                    }.frame(width: 136)
+                    Spacer()
+                    Button(action: {
+                        if(tm.synthesizer.isPaused == true){
+                            tm.synthesizer.continueSpeaking()
+                        } else if (tm.synthesizer.isSpeaking == true){
+                            tm.synthesizer.pauseSpeaking(at: AVSpeechBoundary.immediate)
+                        } else if(!tm.synthesizer.isSpeaking){
+                            tm.speak("Anda dapat menekan tombol pin untuk beralih ke aplikasi navigasi atau tombol kaca pembesar untuk beralih ke aplikasi pencarian.")
                         }
-                }.frame(width: 136)
-                Spacer()
-                Button(action: {
-                    if(tm.synthesizer.isPaused == true){
-                        tm.synthesizer.continueSpeaking()
-                    } else if (tm.synthesizer.isSpeaking == true){
-                        tm.synthesizer.pauseSpeaking(at: AVSpeechBoundary.immediate)
-                    } else if(!tm.synthesizer.isSpeaking){
-                        tm.speak("Anda dapat menekan tombol pin untuk beralih ke aplikasi navigasi atau tombol kaca pembesar untuk beralih ke aplikasi pencarian.")
-                    }
-                }, label: {
-                    Image(systemName: "speaker.wave.2.circle.fill")
-                        .resizable()
-                        .frame(width: 50, height: 50)
-                        .foregroundStyle(Color.button)
-                })
-                .padding(.bottom, 50)
-            }.padding()
-        }.navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button {
-                        dismiss()
-                    } label: {
-                        Image(systemName: "arrow.backward")
-                            .foregroundColor(.button)
+                    }, label: {
+                        Image(systemName: "speaker.wave.2.circle.fill")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                            .foregroundStyle(Color.button)
+                    })
+                    .padding(.bottom, 50)
+                }.padding()
+            }.navigationBarBackButtonHidden(true)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "arrow.backward")
+                                .foregroundColor(.button)
+                        }
                     }
                 }
-            }
-//            .navigationBarBackButtonHidden(true)
-//            .navigationDestination(isPresented: $navigateToMap) {
-//               
-//            }
-        } //akhir navihationstack
-//        .navigationBarBackButtonHidden(true)
+        }.navigationBarBackButtonHidden(true)
         
     }
 }
