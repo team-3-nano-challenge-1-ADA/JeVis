298294308b49487d12873cf739def3c0ea676d73
Rizki Maulana <rizki.maulana@majoo.id>
2024-04-30 15:22:36 +0700

    Fix webview problem

--- JeVis/Views/WebView.swift
+++ JeVis/Views/WebView.swift
@@ -12,15 +12,24 @@ struct WebView: UIViewRepresentable {
     let url: URL
     @Binding var isLoading: Bool
     
+    func makeViewConfiguration() -> WKWebViewConfiguration {
+        let configuration = WKWebViewConfiguration()
+        let dropSharedWorkersScript = WKUserScript(source: "delete window.SharedWorker;", injectionTime: WKUserScriptInjectionTime.atDocumentStart, forMainFrameOnly: false)
+        configuration.userContentController.addUserScript(dropSharedWorkersScript)
+        return configuration
+    }
+    
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
+        let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 400, height: 400), configuration: makeViewConfiguration())
         webView.navigationDelegate = context.coordinator
         return webView
     }
     
     func updateUIView(_ webView: WKWebView, context: Context) {
         let request = URLRequest(url: url)
-        webView.load(request)
+        DispatchQueue.main.async {
+            webView.load(request)
+        }
     }
     
     func makeCoordinator() -> Coordinator {
