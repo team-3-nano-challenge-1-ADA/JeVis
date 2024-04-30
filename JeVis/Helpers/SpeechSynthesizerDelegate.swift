ab2ad467e5b5012e7508b1058f9c53f062ab8b94
Jovanna Melissa <jovannamelissa6@gmail.com>
2024-04-30 14:58:52 +0700

    Stop animation once the speech stops

--- JeVis/Helpers/SpeechSynthesizerDelegate.swift
+++ JeVis/Helpers/SpeechSynthesizerDelegate.swift
@@ -10,10 +10,14 @@ import AVFoundation
 
 class SpeechSynthesizerDelegate: NSObject, AVSpeechSynthesizerDelegate{
     var didFinishSuccess = false
+    var didFinishSpeechCallback: (() -> Void)?
+    
     func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
             // When speech finishes, perform some tasks
             print("Speech synthesis finished for utterance: \(utterance.speechString)")
             didFinishSuccess = true
             print(didFinishSuccess)
+        
+            didFinishSpeechCallback?()
         }
 }
