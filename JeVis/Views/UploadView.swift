1f9078dc925bf985da4ba5e63335de51bce9ffc1
HalizaSyafa27 <111560506+HalizaSyafa27@users.noreply.github.com>
2024-04-30 09:22:15 +0700

    animation upload splash

--- JeVis/Views/UploadView.swift
+++ JeVis/Views/UploadView.swift
@@ -49,15 +49,23 @@ struct UploadView: View {
                                 .padding()
                         }
                     }
-                    Image(systemName: "photo.circle.fill")
-                        .resizable()
-                        .foregroundColor(Color.button)
-                        .frame(width: 100, height: 100)
+//                    Image(systemName: "photo.circle.fill")
+//                        .resizable()
+//                        .foregroundColor(Color.button)
+//                        .frame(width: 100, height: 100)
+//                        .padding(.top, 200)
+//                        .onTapGesture {
+//                            if (self.showProgressBar) {return}
+//                            self.showingPickerOption = true
+//                        }
+                    AnimationView(name: "tap", animationSpeed: 0.5)
+                        .frame(width: 200, height: 200)
                         .padding(.top, 200)
                         .onTapGesture {
-                            if (self.showProgressBar) {return}
+                            if(self.showProgressBar) {return}
                             self.showingPickerOption = true
                         }
+                    
                     Spacer()
                     if showProgressBar {
                         AnimationView(name: "progressBar", animationSpeed: 0.5)
